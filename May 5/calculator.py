6ac029165469e7a1ca3926c55052ed85d7febc45
krishnendhu <krishnendhuvinodkumar393@gmail.com>
2025-05-06 14:11:33 +0530

    Added May 6 projects: palindrome checker, login system, temperature converter

--- May 5/calculator.py
+++ May 5/calculator.py
@@ -1,26 +1,34 @@
+#Function to add 2 numbers
 def add(a,b):
     return a+b
+#Function to subtract 2 numbers
 def sub(a,b):
     return a-b
+#Function to multiply 2 numbers
 def mult(a,b):
     return a*b
+#Function to divide 2 numbers
 def div(a,b):
     if b==0 :
         return "Invalid"
     return a/b
 
+# Prompt user to enter numbers
 print("Enter numbers :")
 a = float(input("Enter a : "))
 b = float(input("Enter b :"))
 
+#Display operations
 print("Select operator")
 print("1. Add")
 print("2. Substract")
 print("3. Multiply")
 print("4. Divide")
 
+#Take a choice from the user 
 choice = input("Enter the choice :")
 
+#Do operation based on the choice
 if choice=='1':
     print(a ,"+" ,b ,"=" , add(a,b))
 elif choice=='2':
--- May 5/fibonacci.py
+++ May 5/fibonacci.py
@@ -1,17 +1,19 @@
+#Take input from the user 
 n= int(input("Enter how many terms you want in fibonacci sequence : "))
-a=0
-b=1
-count=0
+a=0 #Initialize 1st term as 0
+b=1 #Initialize 2nd term as 1
+count=0 # Variable to store the current Fibonacci number
 
+# Check the user input is valid 
 if n<=0 :
     print("Please enter number greater than 0")
 else :
    print("The fibonacci sequence : ")
 
+#Loop to generate and print the Fibonacci sequence
 for i in range(0, n):
     print(count,end=' ')
+    #Update values
     a=b
     b= count
     count = a+b
-
-
